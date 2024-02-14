0981c728d0396d89fc1c6529da276f049131578a
johnkjrfc <johnkjrfc@gmail.com>
2024-02-14 15:38:08 -0600

    updated Job, Employer Ctrlrs

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,14 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotNull
     @Size(min=1, max=80)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> job = new ArrayList<>();
 
     public Employer () {};
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -4,6 +4,8 @@ package org.launchcode.techjobs.persistent.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -14,8 +16,11 @@ public class Job {
     @GeneratedValue
     private int id;
 
+    @ManyToOne
+    @NotNull
+    private Employer employer;
     private String name;
-    private String employer;
+
     private String skills;
 
 
@@ -23,9 +28,9 @@ public class Job {
     }
 
     // Initialize the id and value fields.
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer employer, String someSkills) {
         super();
-        this.employer = anEmployer;
+        this.employer = employer;
         this.skills = someSkills;
     }
 
@@ -39,17 +44,18 @@ public class Job {
         this.name = name;
     }
 
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
     public String getSkills() {
         return skills;
     }
+
 
     public void setSkills(String skills) {
         this.skills = skills;
