d2ca38b90dd4f7dc5a9d6fe055d74c6e94fbfe17
johnkjrfc <johnkjrfc@gmail.com>
2024-02-15 08:31:26 -0600

    made changes to HomeCtrlr

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,31 +20,55 @@ import java.util.Optional;
  */
 @Controller
 public class HomeController {
+    @Autowired//links controller to repo
+    private EmployerRepository employerRepository;
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
 
-        model.addAttribute("title", "MyJobs");
+        model.addAttribute("jobs", jobRepository.findAll());
 
         return "index";
     }
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
-	model.addAttribute("title", "Add Job");
+
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId) {
+                                       Errors errors, Model model, @RequestParam(required = false) Integer employerId) {
 
         if (errors.hasErrors()) {
-	    model.addAttribute("title", "Add Job");
+	        model.addAttribute(new Job());
+
+            return "add";
+        }
+        if (employerId != null){
+            //empId present--> create optEmp, and try to retrieve existing emp from repo based on req param
+            Optional<Employer> optionalEmployer = employerRepository.findById(employerId);
+            if (optionalEmployer.isPresent()) {
+                //if employerId exists in repo, get it and use in newJob
+                Employer employer = optionalEmployer.get();
+                newJob.setEmployer(employer);
+            } else {
+                //employerId on form, not found in repo
+                model.addAttribute("error", "Invalid employer ID");
+                return "add";
+            }
+        } else {
+            //no employerId provided in form
+            model.addAttribute("error", "Employer ID required");
             return "add";
         }
 
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
